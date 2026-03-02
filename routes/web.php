e83438fb4586ae71f40a6026a974ae95fa61de61
Averroes <alfaruqiaverroes@gmail.com>
2026-03-02 09:00:35 +0700

    fix api index and routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', fn() => view('home'));
+Route::get('/', function () {
+    return "Laravel hidup di Vercel";
+});
